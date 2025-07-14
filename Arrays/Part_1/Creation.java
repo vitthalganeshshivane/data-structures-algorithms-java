ea6b2706690489f8f9dbb02c3b7606c4ae2aaa2e
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-14 15:30:39 +0530

    Initial commit: Added arrays folder and README

---
+++ Arrays/Part_1/Creation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Creation {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        int numbers[] = { 45, 4, 5 };
+
+        String names[] = { "Raghuveer", "Ganesh", "Prasad", "Nikhil", "Pankaj" };
+
+    }
+
+}
