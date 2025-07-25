0e7c1fc3de6ecd5324e38e08253a72b967d9ec13
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-25 19:26:10 +0530

    Bubble Sort

---
+++ Basic_Sorting_Algorithms/BubbleSort.java
@@ -0,0 +1,30 @@
+package Basic_Sorting_Algorithms;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        for (int turn = 0; turn < arr.length; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
