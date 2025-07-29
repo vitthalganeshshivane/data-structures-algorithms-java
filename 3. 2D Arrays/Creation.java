bf6f1ef3fe0f9c501402c81ad32704d294d6cdbb
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-29 18:38:08 +0530

    Creation of 2d arrays

---
+++ 3. 2D Arrays/Creation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Creation {
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+        int m = matrix.length, n = matrix[0].length;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
