a341ffe4df20c1db55582dca88b4f6496ce7e381
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-04 17:12:32 +0530

    Comparison function for Strings

---
+++ 4. Strings/Comparisons.java
@@ -0,0 +1,13 @@
+public class Comparisons {
+    public static void main(String args[]) {
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if (s1.equals(s3)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
