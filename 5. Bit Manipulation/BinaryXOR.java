abd6addd09bbcaf34913e9d294c3b7b2b755300d
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-08 19:10:29 +0530

    the use of binary XOR -> ^ operator

---
+++ 5. Bit Manipulation/BinaryXOR.java
@@ -0,0 +1,8 @@
+public class BinaryXOR {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 6;
+        int xor = a ^ b;
+        System.out.println("the value of a ^ b is: " + xor);
+    }
+}
