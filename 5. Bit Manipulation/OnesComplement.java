0e01548094b7a484caae0bebc3ac576db4b12a49
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-08 19:27:53 +0530

    one's complement ~

---
+++ 5. Bit Manipulation/OnesComplement.java
@@ -0,0 +1,7 @@
+public class OnesComplement {
+    public static void main(String args[]) {
+        int a = 5;
+        int complement = ~a;
+        System.out.println("The ones complement of a is: " + complement);
+    }
+}
