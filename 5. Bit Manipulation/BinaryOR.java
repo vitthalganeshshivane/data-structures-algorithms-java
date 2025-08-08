cc5d92e95e125d1eb14ee992c5ce7e2f319d6a37
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-08 19:05:31 +0530

    the use of binary or -> | operator

---
+++ 5. Bit Manipulation/BinaryOR.java
@@ -0,0 +1,8 @@
+public class BinaryOR {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 6;
+        int or = a | b;
+        System.out.println("the value of a | b is: " + or);
+    }
+}
