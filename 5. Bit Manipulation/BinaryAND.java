14b88a5d48a9317447cac982eab601afd4bc27c1
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-08 15:46:01 +0530

    the use of binary and -> & operator

---
+++ 5. Bit Manipulation/BinaryAND.java
@@ -0,0 +1,8 @@
+public class BinaryAND {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 6;
+        int and = a & b;
+        System.out.println("the value of a & b is: " + and);
+    }
+}
