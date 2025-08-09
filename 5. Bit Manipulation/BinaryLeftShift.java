edd8dd16217616cb1495bf5272e2c2719ab20379
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-09 11:43:59 +0530

    The use of binary left shift -> << operaotr

---
+++ 5. Bit Manipulation/BinaryLeftShift.java
@@ -0,0 +1,8 @@
+public class BinaryLeftShift {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 2;
+        int leftShift = a << b;
+        System.out.println("a << b: " + leftShift);
+    }
+}
