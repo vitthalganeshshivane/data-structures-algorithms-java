a3c870ba52e4e6d841e6dddcf633e0887bb177c1
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-09 11:52:28 +0530

    The use of binary right shift -> >> operaotr

---
+++ 5. Bit Manipulation/BinaryRightShift.java
@@ -0,0 +1,8 @@
+public class BinaryRightShift {
+    public static void main(String args[]) {
+        int a = 6;
+        int b = 1;
+        int rightShift = a >> b;
+        System.out.println("a >> b: " + rightShift);
+    }
+}
