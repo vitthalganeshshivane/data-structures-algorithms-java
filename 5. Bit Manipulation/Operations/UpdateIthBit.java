c02f2500e3a2d5d230edffec2382f7fc9e7941fb
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-10 14:01:45 +0530

    Upadate ith bit

---
+++ 5. Bit Manipulation/Operations/UpdateIthBit.java
@@ -0,0 +1,31 @@
+package Operations;
+
+public class UpdateIthBit {
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        // if(newBit == 0) {
+        // return(clearIthBit(n, i));
+        // } else {
+        // return setIthBit(n, i)
+        // }
+
+        n = clearIthBit(n, i);
+        int BitMask = newBit << i;
+        return n | BitMask;
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println(updateIthBit(10, 2, 1));
+    }
+}
