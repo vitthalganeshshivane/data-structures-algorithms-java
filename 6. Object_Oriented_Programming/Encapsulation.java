714d27cb7f83c925d38c8d2049424a25b9baddb4
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-12 14:05:56 +0530

    Encapsulation in java

---
+++ 6. Object_Oriented_Programming/Encapsulation.java
@@ -0,0 +1,7 @@
+// Encapsulation is defined as the wrapping up of data & methods under a single unit. it also implements data hiding.
+
+public class Encapsulation {
+    public static void main(String args[]) {
+        System.out.println("Encapsulation");
+    }
+}
