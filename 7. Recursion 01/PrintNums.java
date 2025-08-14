0c0a4645ad185db55ccd03f697db0d441bac1e50
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-14 13:03:19 +0530

    Print n numbers in Increasing order by using recursion

--- 7. Recursion 01/PrintNums.java
+++ 7. Recursion 01/PrintNums.java
@@ -3,6 +3,7 @@ public class PrintNums {
     public static void printDec(int n) {
         if (n == 1) {
             System.out.print(n + " ");
+            System.out.println();
             return;
         }
         System.out.print(n + " ");
@@ -10,8 +11,18 @@ public class PrintNums {
 
     }
 
+    public static void printInc(int n) {
+        if (n == 10) {
+            System.out.print(n + " ");
+            return;
+        }
+        System.out.print(n + " ");
+        printInc(n + 1);
+    }
+
     public static void main(String args[]) {
         int n = 10;
         printDec(n);
+        printInc(1);
     }
 }
