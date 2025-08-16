f483a0b4768c79b90eb4875bfbb49b8019ecb157
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-16 18:35:18 +0530

    more optimized with O(log n)print x to the power of n by using recursion

--- 7. Recursion 01/PowerN.java
+++ 7. Recursion 01/PowerN.java
@@ -1,5 +1,6 @@
 public class PowerN {
 
+    // brute force approach function
     public static int power(int x, int n) {
         if (n == 0) {
             return 1;
@@ -11,7 +12,24 @@ public class PowerN {
         return x * power(x, n - 1);
     }
 
+    // optimized function with O(logn)
+    public static int optiPower(int a, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPowerSq = optiPower(a, n / 2) * optiPower(a, n / 2);
+        // n is odd
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
+
     public static void main(String args[]) {
         System.out.println(power(2, 10));
+        System.out.println(optiPower(2, 10));
     }
 }
