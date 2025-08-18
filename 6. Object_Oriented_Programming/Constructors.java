7fdd8342d7b09d990164717bcfc707800e643b97
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-18 13:18:05 +0530

    Shallow copy and deep copy in constructors

--- 6. Object_Oriented_Programming/Constructors.java
+++ 6. Object_Oriented_Programming/Constructors.java
@@ -43,11 +43,23 @@ public class Constructors {
         String password;
         int marks[];
 
+        // shallow copy constructor
+        // Student(Student s1) {
+        // marks = new int[3];
+        // this.name = s1.name;
+        // this.roll = s1.roll;
+        // this.marks = s1.marks;
+        // }
+
+        // deep copy constructor
         Student(Student s1) {
             marks = new int[3];
             this.name = s1.name;
             this.roll = s1.roll;
             this.marks = s1.marks;
+            for (int i = 0; i < marks.length; i++) {
+                this.marks[i] = s1.marks[i];
+            }
         }
 
         Student() {
