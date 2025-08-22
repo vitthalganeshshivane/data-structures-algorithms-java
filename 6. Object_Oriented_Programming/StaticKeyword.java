78b78836697782e8f7f0075c6077379efb98ee91
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-22 18:16:17 +0530

    Static Keyword in java

---
+++ 6. Object_Oriented_Programming/StaticKeyword.java
@@ -0,0 +1,21 @@
+public class StaticKeyword {
+
+    class Student {
+        String name;
+        int roll;
+
+        static String schoolName;
+
+        void setName(String name) {
+            this.name = name;
+        }
+
+        String getName() {
+            return this.name;
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
