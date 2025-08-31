a4bd87842606fa0ef07967b98875f57b1b1aa9d2
vitthal4545 <vitthalgs1434@gmail.com>
2025-08-31 15:49:38 +0530

    Print the Reverse Arraylist

---
+++ 11. ArrayList/ReverseArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ReverseArrayList {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+
+        // reverse print
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.err.println(list.get(i));
+        }
+    }
+}
